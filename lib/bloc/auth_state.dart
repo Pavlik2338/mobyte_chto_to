b0b2efbd1f4b43638bcce8a5691d3517315d738c
Pavlik2338 <100232187+Pavlik2338@users.noreply.github.com>
2022-07-28 13:35:18 +0300

    wow

---
+++ lib/bloc/auth_state.dart
@@ -0,0 +1,11 @@
+part of 'auth_bloc.dart';
+
+@immutable
+abstract class AuthState {}
+
+class AuthInitial extends AuthState {
+  final bool? error;
+  final String errorText;
+  final bool? success;
+  AuthInitial({this.error = false, this.errorText = "", this.success = false});
+}
